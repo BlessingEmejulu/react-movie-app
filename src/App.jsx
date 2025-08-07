1d7b8e6810bb44fdf50b87028f140b06dd6e321a
BlessingEmejulu <blessingnneamaka57@gmail.com>
2025-08-06 20:40:00 -0700

    Add initial project setup with assets, styles, and configuration

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main>
+      <div className='pattern' />
+
+      <div className='wrapper'>
+        <header>
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy without the Hassel</h1>
+        </header>
+
+      </div>
+    </main>
+ 
+  );
+}
+
+export default App;
