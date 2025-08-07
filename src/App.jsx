db8ca714b0c48eed4f42a57540325475de6b0e89
BlessingEmejulu <blessingnneamaka57@gmail.com>
2025-08-07 01:51:45 -0700

    designed the  header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Search from './components/Search'
 
 function App() {
+  const [searhTerm, setSearchTerm] = useState('');
   return (
     <main>
       <div className='pattern' />
 
       <div className='wrapper'>
         <header>
+          <img src="/hero-img.png" alt="hero-banner" />
           <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy without the Hassel</h1>
         </header>
-
+        <Search searhTerm={searhTerm} setSearchTerm={setSearchTerm} />
       </div>
     </main>
- 
+
   );
 }
 
